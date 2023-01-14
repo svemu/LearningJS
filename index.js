a6cf2e083404059f9a2fb81dc605aaf2d00388be
Sharanya Vemu <svemu@github.com>
2023-01-14 11:42:17 +0530

    JS learning file for basics

---
+++ index.js
@@ -0,0 +1,27 @@
+// Simple Console print statement
+
+console.log("Hello World!");
+
+// Declare variables
+
+var a = 10;
+let b = 20;
+const c = 30;
+
+// Print variables
+console.log(a);
+console.log(b);
+console.log(c);
+
+//Change variable value
+
+a = 100;
+b = 200;
+//c = 300; // Error: Assignment to constant variable.
+
+// Print variables
+console.log(a);
+console.log(b);
+console.log(c);
+
+
